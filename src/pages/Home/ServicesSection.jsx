5a86236963b073b3c631ceb8b9ed2f78d4a6d6bd
riaz9191 <riazahammed9191@gmail.com>
2024-09-17 23:31:55 +0600

    servicess

--- src/pages/Home/ServicesSection.jsx
+++ src/pages/Home/ServicesSection.jsx
@@ -6,7 +6,7 @@ const ServicesSection = () => {
     <div className="bg-gradient-to-r from-[#EC3339] to-[#BB1117%] py-10">
       <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 text-white text-center">
         <div>
-          <FaShippingFast size={40} className="mx-auto mb-4" />
+          <FaShippingFast size={40} className="mx-auto mb-4 " />
           <h3 className="text-xl font-bold mb-2">Shipping</h3>
           <p className="text-sm">
             The attention to detail is evident in every aspect of this backpack.
